0614c17976a1e70ef9db0ddfba3dbb17e8bc3bd9
Amit Sahu <amitsahu174507@gmail.com>
2024-07-02 23:55:50 +0530

    ..

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-/// let  resume our Music World
+
+console.log("Welcome to Spotify : The World of Music");
+
+
+// Here Initializing the variables 
+let songIndex = 0;
+let audioElement = new Audio('1.mp3');
+let masterPlay = document.getElementById('masterPlay');
+
+
+let songs = [
+    {songName: "Brown Munde",filePath: "songs/1.mp3",coverPath:"covers/1.jpg"},
+    {songName: "Brown Munde",filePath: "songs/1.mp3",coverPath:"covers/1.jpg"},
+    {songName: "Brown Munde",filePath: "songs/1.mp3",coverPath:"covers/1.jpg"},
+    {songName: "Brown Munde",filePath: "songs/1.mp3",coverPath:"covers/1.jpg"},
+    {songName: "Brown Munde",filePath: "songs/1.mp3",coverPath:"covers/1.jpg"},
+    {songName: "Brown Munde",filePath: "songs/1.mp3",coverPath:"covers/1.jpg"},
+]
+
+//audioElement.play();
+
+//Listen to Events 
