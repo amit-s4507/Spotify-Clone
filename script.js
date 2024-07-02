c1926811a0f9234823f33f1fbda425498b00fa92
Amit Sahu <amitsahu174507@gmail.com>
2024-07-03 00:14:03 +0530

    ..

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ console.log("Welcome to Spotify : The World of Music");
 let songIndex = 0;
 let audioElement = new Audio('1.mp3');
 let masterPlay = document.getElementById('masterPlay');
-
+let myProgessBar = document.getElementById('myProgressBar');
 
 let songs = [
     {songName: "Brown Munde",filePath: "songs/1.mp3",coverPath:"covers/1.jpg"},
@@ -15,8 +15,23 @@ let songs = [
     {songName: "Brown Munde",filePath: "songs/1.mp3",coverPath:"covers/1.jpg"},
     {songName: "Brown Munde",filePath: "songs/1.mp3",coverPath:"covers/1.jpg"},
     {songName: "Brown Munde",filePath: "songs/1.mp3",coverPath:"covers/1.jpg"},
-]
+];
 
 //audioElement.play();
 
+// Handle play/pause click
+
+masterPlay.addEventListener('click', () => {
+    if (audioElement.paused || audioElement.currentTime <= 0) {
+        audioElement.play();
+        masterPlay.classList.remove('fa-play-circle');
+        masterPlay.classList.add('fa-pause-circle');
+    }
+});
+
 //Listen to Events 
+myProgressBar.addEventListener('timeupdate',()=>{
+
+    console.log('timeupdate');
+
+})
